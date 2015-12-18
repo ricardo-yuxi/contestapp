28f3baba9c6a41132125aa4c13cf4438f38d05d6
Ricardo Rojas <ricardo.rojas@Pablos-iMac.local>
2015-12-18 16:34:06 -0500

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
+  root 'dashboard/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
